6da1cf7fb07fe241dcd7364c84ffbe5b0fc252ac
Hasan <hasan.softbdltd@gmail.com>
2021-07-18 11:51:44 +0600

    Basic project setup

--- routes/web.php
+++ routes/web.php
@@ -9,37 +9,10 @@ $customRouter = function (string $as = '') use ($router) {
     return $custom->as($as);
 };
 
-
-$router->get('/hello', 'ExampleController@hateoasResponse');
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-
 $router->group(['prefix' => 'api/v1', 'as' => 'api.v1'], function () use ($router, $customRouter) {
-
-    //ranks crud operation
-    $customRouter()->resourceRoute('ranks', 'RankController')->render();
-
-    //ranktypes crud operation
-    $customRouter()->resourceRoute('rank-types', 'RankTypeController')->render();
-
-    //jobsectors crud operation
-    $customRouter()->resourceRoute('job-sectors', 'JobSectorController')->render();
-
-    //skills crud operation
-    $customRouter()->resourceRoute('skills', 'SkillController')->render();
-
-    //occupation crud api
-    $customRouter()->resourceRoute('occupations', 'OccupationController')->render();
-
-
-    //organizationsTypes crud operation
-    $customRouter()->resourceRoute('organization-types', 'OrganizationTypeController')->render();
-
-    //organization crud operation
     $customRouter()->resourceRoute('organizations', 'OrganizationController')->render();
-
-
 });
