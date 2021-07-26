ad3c625b2e549e0838ee588efdf5983feabd2f4c
Miladul Islam <imiladul@gmail.com>
2021-07-26 09:25:38 +0600

    Branch Model fix

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -11,9 +12,13 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property string title_en
  * @property string title_bn
  * @property int institute_id
- * @property int row_status
  * @property string|null address
  * @property string|null google_map_src
+ * @method static Builder|Institute acl()
+ * @method static Builder|Institute active()
+ * @method static Builder|Institute newModelQuery()
+ * @method static Builder|Institute newQuery()
+ * @method static Builder|Institute query()
  */
 
 class Branch extends BaseModel
@@ -24,4 +29,11 @@ class Branch extends BaseModel
     {
         return $this->belongsTo(Institute::class);
     }
+
+    public function courseConfigs(): HasMany
+    {
+        return $this->hasMany(CourseConfig::class);
+    }
+
+
 }
