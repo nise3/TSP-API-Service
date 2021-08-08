6642dff1be20ae12a8037729b5665776426dde5f
ummek <alshahadath@gmail.com>
2021-08-08 15:35:32 +0600

    fix/shahadath_institutes

---
+++ database/factories/InstituteFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+
+use App\Models\Institute;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class InstituteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Institute::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        $instituteName = $this->faker->company;
+        return [
+            'title_en' => $instituteName,
+            'title_bn' => $instituteName,
+            'code' => $this->faker->unique(),
+            'domain' => 'https://' . $this->faker->domainName,
+            'address' => $this->faker->address,
+        ];
+    }
+}
