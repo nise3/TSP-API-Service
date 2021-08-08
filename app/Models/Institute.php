bab80225010a27ae87852886c35dfa0ddfe160f7
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-08-08 15:41:05 +0600

    small changes

--- app/Models/Institute.php
+++ app/Models/Institute.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
 /**
  * Class Institute
  * @package App\Models
@@ -17,24 +18,33 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property string|null google_map_src
  * @property string logo
  * @property string|null config
- * @method static Builder|Institute newModelQuery()
- * @method static Builder|Institute newQuery()
- * @method static Builder|Institute query()
  */
 class Institute extends BaseModel
 {
 
     use SoftDeletes;
 
+    /**
+     * @var string[]
+     */
     protected $guarded = ['id'];
 
+    /**
+     *
+     */
     const DEFAULT_LOGO = 'institute/default.jpg';
 
+    /**
+     * @var string[]
+     */
     protected $casts = [
         'phone_numbers' => 'array',
         'mobile_numbers' => 'array',
     ];
 
+    /**
+     * @return string|null
+     */
     public function title(): ?string
     {
         return $this->title_bn || $this->title_en;
