fbee57af50851679a1ed8b7f69778a6bc9fef3d5
Taskinur Rahman <taskinur.rahman2014@gmail.com>
2021-08-11 16:04:17 +0600

    fix training center and seeder

--- database/factories/TrainingCenterFactory.php
+++ database/factories/TrainingCenterFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Branch;
 use App\Models\TrainingCenter;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -9,13 +10,15 @@ class TrainingCenterFactory extends Factory
 {
     protected $model = TrainingCenter::class;
 
+
     public function definition(): array
     {
+        $branch = Branch::all()->random();
     	return [
             'title_en' => $this->faker->name,
             'title_bn' => $this->faker->name,
             'institute_id' => $this->faker->randomElement([1,2,3,4,5,6,7,8,9,10]),
-//            'branch_id' => $this->faker->randomElement([1,2,3,4,5,6,7,8,9,10]),
+            'branch_id' => $branch->id,
             'address' => $this->faker->address,
     	];
     }
