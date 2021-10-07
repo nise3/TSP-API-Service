aad4c4215f94208e5d3226d9d808ad893ba615d0
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-07 15:34:51 +0600

    Seeder with location (lat, long) data completed

--- database/factories/TrainingCenterFactory.php
+++ database/factories/TrainingCenterFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\TrainingCenter;
+use App\Services\LocationSeederHelper;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TrainingCenterFactory extends Factory
@@ -10,14 +11,23 @@ class TrainingCenterFactory extends Factory
     protected $model = TrainingCenter::class;
 
 
+    /**
+     * @throws \Exception
+     */
     public function definition(): array
     {
+        $len = count(LocationSeederHelper::$data);
+        $index = random_int(0, $len - 1);
+        $location = LocationSeederHelper::$data[$index];
+
         return [
             'title_en' => $this->faker->name,
             'title' => $this->faker->name,
-            'loc_division_id' => "1",
-            'loc_district_id' => "1",
-            'loc_upazila_id' => "18",
+            'loc_division_id' => $location['loc_division_id'],
+            'loc_district_id' => $location['loc_district_id'],
+            'loc_upazila_id' => $location['loc_upazila_id'],
+            'location_latitude' => $location['location_longitude'],
+            'location_longitude' => $location['location_longitude'],
             'address' => $this->faker->address,
             'address_en' => $this->faker->address,
             'center_location_type' => $this->faker->randomElement([1, 2, 3]),
