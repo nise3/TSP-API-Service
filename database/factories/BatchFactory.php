d43bf36d501091865404bcc19581eddb8e0d30d7
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-07 11:31:04 +0600

    Batch Factory Updated

--- database/factories/BatchFactory.php
+++ database/factories/BatchFactory.php
@@ -3,21 +3,31 @@
 namespace Database\Factories;
 
 use App\Models\Batch;
-use App\Models\TrainingCenter;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 
 class BatchFactory extends Factory
 {
     protected $model = Batch::class;
 
+    /**
+     * @throws \Exception
+     */
     public function definition(): array
     {
+        /** @var Carbon $registrationStartDate */
+        $registrationStartDate = $this->faker->dateTimeBetween('-8 months', '-1 months');
+        $intRand = random_int(7, 30);
+        $registrationEndDate = $registrationStartDate->addDays($intRand);
+        $intRand = random_int(7, 14);
+        $batchStartDate = $registrationEndDate->addDays($intRand);
+        $batchEndDate = $batchStartDate->addDays(60);
         return [
             'number_of_seats' => $this->faker->numberBetween(30, 100),
-            'registration_start_date' => $this->faker->date('2021-7-21'),
-            'registration_end_date' => $this->faker->date('2021-8-11'),
-            'batch_start_date' => $this->faker->date('2021-8-21'),
-            'batch_end_date' => $this->faker->date('2021-11-21')
+            'registration_start_date' => $registrationStartDate,
+            'registration_end_date' => $registrationEndDate,
+            'batch_start_date' => $batchStartDate,
+            'batch_end_date' => $batchEndDate
         ];
     }
 
