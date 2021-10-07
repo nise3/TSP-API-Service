8481d1a4be3b272bd6ac2847726e2e1f946ad022
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-07 14:23:51 +0600

    Refactoring on going

--- database/seeders/CourseEnrollmentSeeder.php
+++ database/seeders/CourseEnrollmentSeeder.php
@@ -22,13 +22,20 @@ class CourseEnrollmentSeeder extends Seeder
         Schema::disableForeignKeyConstraints();
 
         try {
-            $institutes = Institute::with(['courses','courses.program', 'courses.batches', 'courses.batches.trainingCenter'])->limit(6)->get();
-            foreach ($institutes as $institute){
+            $institutes = Institute::with(['courses', 'courses.program', 'courses.batches', 'courses.batches.trainingCenter'])->limit(6)->get();
+            foreach ($institutes as $institute) {
                 $courseEnrollment = new CourseEnrollment();
                 $courseEnrollment->institute_id = $institute->id;
-                $courseEnrollment->branch_id = $institute->branches[0]->id;
-                $courseEnrollment->program_id = $institute->programs[0]->id;
-                $courseEnrollment->course_id = $institute->courses[0]->id;
+                $index = random_int(0, $institute->branches->count() - 1);
+                $branch = $institute->branches->get($index);
+                $courseEnrollment->branch_id = $branch->id;
+
+                $index = random_int(0, $institute->courses->count() - 1);
+                $course = $institute->courses->get($index);
+
+                $courseEnrollment->program_id = $course->program_id;
+                $courseEnrollment->course_id = $course->id;
+
                 $courseEnrollment->youth_id = random_int(1, 5);
                 $courseEnrollment->save();
             }
