e4c595797857a89926f2c697e0cfa099c4c02962
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-07 15:36:34 +0600

    Seeder with location (lat, long) data completed

--- app/Models/CourseEnrollment.php
+++ app/Models/CourseEnrollment.php
@@ -2,6 +2,13 @@
 
 namespace App\Models;
 
+/**
+ * Class Institute
+ * @package App\Models
+ * @property int id
+ * @property int course_id
+ * @property int youth_id
+ */
 class CourseEnrollment extends BaseModel
 {
     protected $guarded = ['id'];
