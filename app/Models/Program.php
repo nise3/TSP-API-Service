da2b565bf2c85a406bd3991f6d0a5fd7aff7c2c5
rrahul <rahulbgc21@gmail.com>
2021-10-17 15:00:36 +0600

    refactoring

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -3,10 +3,8 @@
 namespace App\Models;
 
 use App\Traits\Scopes\ScopeRowStatusTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Program
