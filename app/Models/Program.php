362459d0e68902380191d0b9f183d93d5a62d301
rrahul <rahulbgc21@gmail.com>
2021-10-17 14:58:47 +0600

    refactoring

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\Scopescopes\ScopeRowStatusTrait;
+use App\Traits\Scopes\ScopeRowStatusTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
