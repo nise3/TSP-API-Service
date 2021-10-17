e65e950e2165e733ca86b6a1a3a645f79ebdffe9
rrahul <rahulbgc21@gmail.com>
2021-10-17 14:58:09 +0600

    refactoring

--- app/Models/TrainingCenter.php
+++ app/Models/TrainingCenter.php
@@ -2,11 +2,8 @@
 
 namespace App\Models;
 
-use App\Traits\scopes\ScopeRowStatusTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class TrainingCenter
@@ -24,7 +21,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class TrainingCenter extends BaseModel
 {
-    use ScopeRowStatusTrait;
     /**
      * @var string[]
      */
