689214c752b62669627c6f008c2cf428fabc175c
rrahul <rahulbgc21@gmail.com>
2021-10-24 17:03:56 +0600

    debug mode false set in nise3 config

--- config/nise3.php
+++ config/nise3.php
@@ -4,7 +4,7 @@ use App\Models\EducationLevel;
 
 return [
     "is_dev_mode" => env("IS_DEVELOPMENT_MODE", false),
-    'http_debug' => env("HTTP_DEBUG_MODE", true),
+    'http_debug' => env("HTTP_DEBUG_MODE", false),
     "should_ssl_verify" => env("IS_SSL_VERIFY", false),
     "http_timeout" => env("HTTP_TIMEOUT", 60),
 
