966a05a9022b2e5b29cd261b18cc8eb327cba59b
Md Tasmidur Rahman <tasmidurrahman@gmail.com>
2021-11-28 19:57:45 +0600

    Url change

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -59,6 +59,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $e): JsonResponse
     {
+
         $errors = [
             '_response_status' => [
                 'success' => false,
@@ -112,7 +113,7 @@ class Handler extends ExceptionHandler
             $errors['_response_status']['code'] = ResponseAlias::HTTP_INTERNAL_SERVER_ERROR;
             $errors['_response_status']['message'] = $e->getMessage();
         } elseif ($e instanceof Exception) {
-            $errors['_response_status']['code'] = $e->getCode() ?? ResponseAlias::HTTP_INTERNAL_SERVER_ERROR;
+            $errors['_response_status']['code'] = ($e->getCode() || $e->getCode() == 0) ?? ResponseAlias::HTTP_INTERNAL_SERVER_ERROR;
             $errors['_response_status']['message'] = $e->getMessage();
         }
         return response()->json($errors, $errors['_response_status']['code']);
