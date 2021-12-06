0bda6dcbc72f1a531d2c0b0b36117bda4c75abc7
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-12-06 18:48:38 +0600

    Course filter added in course-list API

--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -737,6 +737,21 @@ class CourseService
             $coursesBuilder->where('courses.level', '=', $courseLevel);
         }
 
+        if ($type == self::COURSE_FILTER_RECENT || is_numeric($availability)) {
+            if ($type == self::COURSE_FILTER_RECENT || $availability == self::COURSE_FILTER_AVAILABILITY_RUNNING) {
+                $coursesBuilder->where(function($builder) use($curDate){
+                    $builder->whereDate('batches.registration_start_date', '<=', $curDate);
+                    $builder->whereDate('batches.registration_end_date', '>=', $curDate);
+                });
+            }
+            if ($availability == self::COURSE_FILTER_AVAILABILITY_UPCOMING) {
+                $coursesBuilder->WhereDate('batches.registration_start_date', '>', $curDate);
+            }
+            if ($availability == self::COURSE_FILTER_AVAILABILITY_COMPLETED) {
+                $coursesBuilder->whereDate('batches.batch_end_date', '<', $curDate);
+            }
+        }
+
         $coursesBuilder->groupBy("courses.id");
         $coursesBuilder->orderByDesc('total_enroll');
 
