8e8656b2e23018001e7675f128315cc6595735f9
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-12-09 01:23:05 +0600

    Modify Facade & Folder structure

--- app/Models/SagaErrorEvent.php
+++ app/Models/SagaErrorEvent.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SagaErrorEvent extends Model
+class SagaErrorEvent extends BaseModel
 {
     protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_SIMPLE;
 }
