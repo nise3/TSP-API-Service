d4983704c0763aa43d42940ca6da8abddf714572
rrahul <rahulbgc21@gmail.com>
2021-12-08 16:41:10 +0600

    refactoring

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -16,7 +16,7 @@ abstract class BasePolicy
     public function before($authUser, $ability) : bool
     {
         /** @var User $authUser */
-        if ($authUser->row_status != User::ROW_STATUS_ACTIVE) {
+        if ($authUser && $authUser->row_status != User::ROW_STATUS_ACTIVE) {
             return false;
         }
     }
