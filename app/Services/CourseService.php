242e42fa3678980175623b09527d8f644d0db1c1
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-09 15:13:24 +0600

    Added log for Youth Enrolled courses payment_status & verified

--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -792,10 +792,9 @@ class CourseService
 
         } else {
             $courses = $coursesBuilder->get();
+            Log::info("The courses");
+            Log::info(json_encode($courses));
         }
-
-        Log::info("The courses");
-        Log::info(json_encode($courses));
 
         /** Set course already enrolled OR not for youth. Also set payment_status & verified fields if youth enrolled in a course */
         if (is_numeric($youthId)) {
