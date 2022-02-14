0202bdec0b50dfd8f233b62afbbd935e91db6c60
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-14 13:12:59 +0600

    RELEASE = 'php artisan list' New Open API definition for Payment Gateway IPN endpoints Elasticsearch configured as log storage in dev server

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -120,7 +120,6 @@ abstract class BaseModel extends Model
     public const PASSWORD_REGEX = 'regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/';
     public const PASSWORD_VALIDATION_MESSAGE = 'The password must contain at least one uppercase, lowercase letter and at least one number.[66000]';
 
-    public const INSTITUTE_REMOTE_BASE_URL = 'https://institute.bus-staging.softbdltd.com/';
     const INSTITUTE_LOCAL_BASE_URL = "http://localhost:8001/";
     public const NISE3_FROM_EMAIL = "noreply@nise.gov.bd";
     public const SELF_EXCHANGE = 'institute';
--- app/Services/Payment/CourseEnrollmentPaymentService.php
+++ app/Services/Payment/CourseEnrollmentPaymentService.php
@@ -35,12 +35,12 @@ class CourseEnrollmentPaymentService
 
         Log::channel('ek_pay')->info("Course Info for course_id-" . $courseEnrollment->course_id . json_encode($courseInfo));
 
-        $baseUrl = BaseModel::INSTITUTE_REMOTE_BASE_URL;
+
         if (request()->getHost() == 'localhost' || request()->getHost() == '127.0. 0.1') {
             $baseUrl = BaseModel::INSTITUTE_LOCAL_BASE_URL;
         }
 
-        $ipnUri = $baseUrl . "api/v1/course-enrollment/payment-by-ek-pay/ipn-handler/" . Uuid::uuid();
+        $ipnUri = env('API_GATEWAY_BASE_URL', 'https://gateway-dev.nise3.xyz') . "/" . env('PAYMENT_GATEWAY_IPN_ENDPOINT_BASE_URI', 'payment-gateway-ipn') . "/" . env('EK_PAY_IPN_URI', 'course-enrollment/payment-by-ek-pay/ipn-handler') . "/" . Uuid::uuid() . "?apikey=" . env('API_GATEWAY_API_KEY');
 
         /** EN+CourseCode+I=36 is an invoice id */
         $invoicePrefix = CourseEnrollment::INVOICE_PREFIX . $courseInfo->code;
