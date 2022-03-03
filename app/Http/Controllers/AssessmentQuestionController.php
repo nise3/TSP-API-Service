90d2da58fcd30d813b19454b570a686a564a2c52
rrahul <rahulbgc21@gmail.com>
2022-03-03 16:37:18 +0600

    assessment question stuff

---
+++ app/Http/Controllers/AssessmentQuestionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AssessmentQuestion;
+use Illuminate\Http\Request;
+
+class AssessmentQuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AssessmentQuestion  $assessmentQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AssessmentQuestion $assessmentQuestion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AssessmentQuestion  $assessmentQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AssessmentQuestion $assessmentQuestion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AssessmentQuestion  $assessmentQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AssessmentQuestion $assessmentQuestion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AssessmentQuestion  $assessmentQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AssessmentQuestion $assessmentQuestion)
+    {
+        //
+    }
+}
