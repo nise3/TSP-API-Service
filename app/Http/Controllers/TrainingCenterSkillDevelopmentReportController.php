63193d95ba3aa74627e3b129d4b445e782928753
naeemur.softbd <Window controlAboutnaeemur.rahman@softbdltd.com>
2022-03-20 17:49:03 +0600

    TrainingCenterSkillDevelopmentReports migration model controller updated

--- app/Http/Controllers/TrainingCenterSkillDevelopmentReportController.php
+++ app/Http/Controllers/TrainingCenterSkillDevelopmentReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\TrainingCenterSkillDevelopmentReportService;
 use Carbon\Carbon;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
@@ -62,5 +63,115 @@ class TrainingCenterSkillDevelopmentReportController extends Controller
         ];
 
         return Response::json($response, ResponseAlias::HTTP_CREATED);
+    }
+
+
+
+    /**
+     * @param Request $request
+     * return use Illuminate\Support\Facades\Validator;
+     * @return Validator
+     */
+    public function validator(Request $request): Validator
+    {
+        $request->offsetSet('deleted_at', null);
+        $data = $request->all();
+
+        $customMessage = [];
+
+        $rules = [
+            'institute_id' => [
+                'required',
+                'int',
+                'min:1',
+                'exists:institutes,id,deleted_at,NULL',
+            ],
+            'training_center_id' => [
+                'required',
+                'int',
+                'min:1',
+                'exists:training_centers,id,deleted_at,NULL',
+            ],
+            'reporting_month' => [
+                'nullable',
+                'date',
+            ],
+            'number_of_trades_allowed' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'number_of_ongoing_trades' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'number_of_computers' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'number_of_other_equipments' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'amount_of_total_fdr' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'current_session_trainees_women' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'current_session_trainees_men' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'current_session_trainees_disabled_and_others' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'total_trainees_women' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'total_trainees_men' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'total_trainees_disabled_and_others' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'bank_status_skill_development' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'bank_status_coordinating_council' => [
+                'nullable',
+                'int',
+                'min:0',
+            ],
+            'date_of_last_election_of_all_party_council' => [
+                'nullable',
+                'date',
+            ],
+            'comments' => [
+                'nullable',
+                'string',
+            ],
+            '' => ['',],
+        ];
+
+        return \Illuminate\Support\Facades\Validator::make($data, $rules, $customMessage);
     }
 }
