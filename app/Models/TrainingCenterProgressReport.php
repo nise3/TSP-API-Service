093f6b1e005a9049e2b743f88cb086fa23c58f2e
taskinur rahman <taskinur.rahman2014@gmail.com>
2022-03-21 17:50:49 +0600

    completed training center progress apis

--- app/Models/TrainingCenterProgressReport.php
+++ app/Models/TrainingCenterProgressReport.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class TrainingCenterProgressReport extends Model
+class TrainingCenterProgressReport extends BaseModel
 {
-    //
+    protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_SIMPLE;
 }
--- database/migrations/2022_03_21_121232_create_training_center_progress_reports_table.php
+++ database/migrations/2022_03_21_121232_create_training_center_progress_reports_table.php
@@ -15,6 +15,46 @@ class CreateTrainingCenterProgressReportsTable extends Migration
     {
         Schema::create('training_center_progress_reports', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('institute_id');
+            $table->unsignedInteger('training_center_id');
+            $table->dateTime('reporting_month');
+            $table->string('trade_name', 200)->nullable();
+            $table->unsignedInteger('number_of_trainers')->default(0);
+            $table->unsignedInteger('number_of_labs_or_training_rooms')->default(0);
+            $table->unsignedInteger('number_of_computers_or_training_equipments')->default(0);
+
+            $table->unsignedInteger('admitted_trainee_men')->default(0);
+            $table->unsignedInteger('admitted_trainee_women')->default(0);
+            $table->unsignedInteger('admitted_trainee_disabled')->default(0);
+            $table->unsignedInteger('admitted_trainee_qawmi')->default(0);
+            $table->unsignedInteger('admitted_trainee_transgender')->default(0);
+            $table->unsignedInteger('admitted_trainee_others')->default(0);
+            $table->unsignedInteger('admitted_trainee_total')->default(0);
+
+            $table->unsignedInteger('technical_board_registered_trainee_men')->default(0);
+            $table->unsignedInteger('technical_board_registered_trainee_women')->default(0);
+            $table->unsignedInteger('technical_board_registered_trainee_disabled')->default(0);
+            $table->unsignedInteger('technical_board_registered_trainee_qawmi')->default(0);
+            $table->unsignedInteger('technical_board_registered_trainee_transgender')->default(0);
+            $table->unsignedInteger('technical_board_registered_trainee_others')->default(0);
+            $table->unsignedInteger('technical_board_registered_trainee_total')->default(0);
+
+            $table->unsignedInteger('latest_test_attended_trainee_men')->default(0);
+            $table->unsignedInteger('latest_test_attended_trainee_women')->default(0);
+            $table->unsignedInteger('latest_test_attended_trainee_disabled')->default(0);
+            $table->unsignedInteger('latest_test_attended_trainee_qawmi')->default(0);
+            $table->unsignedInteger('latest_test_attended_trainee_transgender')->default(0);
+            $table->unsignedInteger('latest_test_attended_trainee_others')->default(0);
+            $table->unsignedInteger('latest_test_attended_trainee_total')->default(0);
+
+            $table->unsignedInteger('latest_test_passed_trainee_men')->default(0);
+            $table->unsignedInteger('latest_test_passed_trainee_women')->default(0);
+            $table->unsignedInteger('latest_test_passed_trainee_disabled')->default(0);
+            $table->unsignedInteger('latest_test_passed_trainee_qawmi')->default(0);
+            $table->unsignedInteger('latest_test_passed_trainee_transgender')->default(0);
+            $table->unsignedInteger('latest_test_passed_trainee_others')->default(0);
+            $table->unsignedInteger('latest_test_passed_trainee_total')->default(0);
+
             $table->timestamps();
         });
     }
