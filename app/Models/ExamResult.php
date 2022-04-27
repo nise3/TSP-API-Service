c88899bb0cd2627b08889ece0d0ac4e53f66874d
taskinur rahman <taskinur.rahman2014@gmail.com>
2022-04-27 16:16:43 +0600

    type cast exam result file paths

--- app/Models/ExamResult.php
+++ app/Models/ExamResult.php
@@ -8,6 +8,7 @@ class ExamResult extends Model
 {
     protected $casts = [
         "answers" => 'array',
+        "file_paths" => 'array',
     ];
     protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_SIMPLE;
 }
