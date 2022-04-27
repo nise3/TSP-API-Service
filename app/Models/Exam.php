ef64136b7a2e3e656d2d63cebbe3623d9e3ddf55
taskinur rahman <taskinur.rahman2014@gmail.com>
2022-04-27 12:08:29 +0600

    added created by updated by in exam type and exam model

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\CreatedUpdatedBy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Exam extends BaseModel
 {
-    use  SoftDeletes;
+    use  SoftDeletes, CreatedUpdatedBy;
 
     protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_SIMPLE_SOFT_DELETE;
     public const EXAM_TYPE_ONLINE = 1;
