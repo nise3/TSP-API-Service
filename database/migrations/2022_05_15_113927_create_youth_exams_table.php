7cad80cd39aac8db89ba8fba27c30b07338719b4
taskinur rahman <taskinur.rahman2014@gmail.com>
2022-05-15 16:31:11 +0600

    updted migration

--- database/migrations/2022_05_15_113927_create_youth_exams_table.php
+++ database/migrations/2022_05_15_113927_create_youth_exams_table.php
@@ -15,9 +15,11 @@ class CreateYouthExamsTable extends Migration
     {
         Schema::create('youth_exams', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('exam_id');
             $table->unsignedInteger('youth_id');
-            $table->unsignedTinyInteger('type')->comment('1=>online, 2=>offline,3=>mixed');
+            $table->unsignedInteger('exam_id')->nullable();
+            $table->unsignedInteger('batch_id');
+            $table->unsignedInteger('exam_type_id');
+            $table->unsignedTinyInteger('type')->comment('1=>online, 2=>offline,3=>mixed,4=>Practical,5=>Field Work,6=>Presentation,7=>Assignment,8=>Assignment');
             $table->json('file_paths')->nullable();
             $table->unsignedDecimal('total_obtained_marks')->nullable();
             $table->timestamps();
