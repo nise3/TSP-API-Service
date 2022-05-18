04a381ddc0f4dd1ee70c7edcf6daa8392e3ca40e
taskinur rahman <taskinur.rahman2014@gmail.com>
2022-05-18 12:34:26 +0600

    small fixing

--- app/Services/BatchService.php
+++ app/Services/BatchService.php
@@ -806,7 +806,7 @@ class BatchService
                 if (is_numeric($youthId)) {
                     $youthExamData = $this->getYouthExamData($id, $youthId, $exam['id']);
                     $exam['obtained_mark'] = $youthExamData->total_obtained ?? 0;
-                    $exam['file_paths'] = $youthExamData->file_paths ?? [];
+                    $exam['file_paths'] = $youthExamData->file_paths ?? null;
 
                 }
             }
