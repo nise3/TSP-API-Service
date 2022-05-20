7a07bfe923dfe1a04bd38c2d85e1077d5c996c03
ehsan munna <grmunnabd@gmail.com>
2022-05-20 16:23:13 +0600

    certificate_issued props added

--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -278,12 +278,14 @@ class CourseService
             $course["trainers"] = $trainers->toArray();
         }
 
+
         if (is_numeric($youthId)) {
             $courseEnrollment = CourseEnrollment::where('course_id', $id)
                 ->where('youth_id', $youthId)
                 ->where('saga_status','!=',BaseModel::SAGA_STATUS_ROLLBACK)
                 ->withoutGlobalScope(SagaStatusGlobalScope::class)
                 ->first();
+
             $course["enrolled"] = (bool)$courseEnrollment;
             $course["payment_status"] = !empty($courseEnrollment) && (bool)$courseEnrollment->payment_status;
             $course["verified"] = !empty($courseEnrollment) && !empty($courseEnrollment->verification_code_verified_at);
