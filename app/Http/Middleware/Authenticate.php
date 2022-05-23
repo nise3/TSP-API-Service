75e65ed59dc83c2e6753918ab7ccddfd7e7e3c17
taskinur rahman <taskinur.rahman2014@gmail.com>
2022-05-23 15:44:27 +0600

    added column in youth my courses and fix auth middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -63,8 +63,12 @@ class Authenticate
 
             } else if ($authUser->isOrganizationUser()) {
                 $request->offsetSet('organization_id', $authUser->organization_id);
+                $request->offsetSet('accessor_type', BaseModel::ACCESSOR_TYPE_ORGANIZATION);
+                $request->offsetSet('accessor_id', $authUser->organization_id);
             } else if ($authUser->isIndustryAssociationUser()) {
                 $request->offsetSet('industry_association_id', $authUser->industry_association_id);
+                $request->offsetSet('accessor_type', BaseModel::ACCESSOR_TYPE_INDUSTRY_ASSOCIATION);
+                $request->offsetSet('accessor_id', $authUser->industry_association_id);
             } else if ($authUser->isRtoUser()) {
                 $request->offsetSet('registered_training_organization_id', $authUser->registered_training_organization_id);
                 $request->offsetSet('rto_id', $authUser->registered_training_organization_id); // TODO: remove this jora tali
